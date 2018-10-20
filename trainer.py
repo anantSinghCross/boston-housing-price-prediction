a39131462392b7205f54ca1ec540eb50a879c323
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2018-10-21 00:32:44 +0530

    Trains the model and pickles it

--- trainer.py
+++ trainer.py
@@ -5,10 +5,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasRegressor
 from sklearn.model_selection import cross_val_score
-from sklearn.datasets import load_boston
 from sklearn.model_selection import KFold
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
 
 # load dataset
 data = pd.read_csv("boston.csv", delimiter = ',')
@@ -20,7 +17,7 @@ print(dataset.shape)
 print(X.shape)
 print(Y.shape)
 
-'''     We can define different Keras Models that will be different 
+'''     Now we can define different Keras Models that will be different 
         with respect to 'number of hidden layers', 'number of epochs' and
         'nuerons in the initial layer'.
 '''
